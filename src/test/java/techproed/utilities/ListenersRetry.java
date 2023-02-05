0718e241c6d2d86021c1df0b7035370ec8d15adf
Asya ilhanli <r.asyailhanli@gmail.com>
2023-02-05 23:22:03 +0300

    son ders

--- src/test/java/techproed/utilities/ListenersRetry.java
+++ src/test/java/techproed/utilities/ListenersRetry.java
@@ -4,8 +4,9 @@ import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
 public class ListenersRetry implements IRetryAnalyzer {
-
+//   BU SINIFIN SADECE FAILE  OLAN TEST CASELERI TEKRAR CALISTIRIR
     private int retryCount = 0;
+//    maxRetryCount EK OLARAK CALISMA SAYISI. BU ORNEKDE FAIL OLAN TESTLER EXTRA 1 KEZ DAHA CALISIR
     private static final int maxRetryCount = 1;
     @Override
     public boolean retry(ITestResult result) {
